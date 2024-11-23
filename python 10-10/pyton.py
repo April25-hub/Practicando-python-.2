5dc7906c8b5871ab61f4a8652d329a7456512101
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-22 19:07:56 -0600

    Add files via upload

---
+++ python 10-10/pyton.py
@@ -0,0 +1,11 @@
+def histograma(lista):
+    for numero in lista:
+        print('*' * numero)
+
+#esta linea dara un ejemplo de uso
+histograma([4, 9, 7])
+# Devuelve:
+# ****
+# *********
+# *******
+

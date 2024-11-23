2b6a0d67cab4f2fd5ebfb141d4cdca1caa86c0b0
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-22 18:46:37 -0600

    Add files via upload

---
+++ python 1.1/python.py
@@ -0,0 +1,9 @@
+def max(a, b):
+    if a > b:
+        return a  # Si 'a' es mayor que 'b', devuelve 'a'
+    else:
+        return b  # Si no, devuelve 'b'
+
+#esta linea dara un jemplo de uso:
+print(max(10, 20))  # Devuelve 20
+print(max(50, 30))  # Devuelve 50
